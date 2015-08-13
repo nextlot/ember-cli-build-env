782c765522e23f190d827d03b4ce4a3ff8f71ec8
Lawrence Davis <lawrence@kohactive.com>
2015-08-13 13:39:16 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 /* jshint node: true */
 'use strict';
 
+var replace = require('broccoli-string-replace');
+var renameFiles = require('broccoli-rename-files');
+var funnel = require('broccoli-funnel');
+
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
+
 module.exports = {
-  name: 'ember-cli-build-env'
+  name: 'ember-cli-build-env',
+
+  treeForPublic: function() {
+    var appEnv = this.app.env;
+    var config = require(this.app.project.root + '/config/environment.js');
+    var appEnvJSON = config(appEnv);
+    var envTree = funnel(unwatchedTree('public'), {
+      srcDir: '/',
+      files: ['env_template.js'],
+      destDir: '/'
+    });
+
+    var replaceTree = replace(envTree, {
+      files: ['env_template.js'],
+      patterns: [{
+        match: /\{\{APP_ENV_JSON\}\}/g,
+        replacement: JSON.stringify(appEnvJSON)
+      }]
+    });
+    
+    var tree = renameFiles(replaceTree, {
+      transformFilename: function() {
+        return 'env.js';
+      }
+    });
+
+    return tree;
+  },
+
+  included: function(app) {
+    this.app = app;
+  }
+
 };
